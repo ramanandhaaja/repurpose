5af35c99c06b63ab8a6346c84f4abea81e6b833e
ramanandhaaja <thisisnandha@gmail.com>
2024-12-19 10:47:16 +0700

    Update page.tsx

--- app/dashboard/youtube-analyzer/page.tsx
+++ app/dashboard/youtube-analyzer/page.tsx
@@ -1,3 +1,5 @@
+use client;
+
 import React, { useState } from 'react';
 import { AlertCircle, CheckCircle } from 'lucide-react';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
